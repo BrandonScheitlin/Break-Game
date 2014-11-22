41f1a6414c2cf1e96168b0a77a773a33dfe131c5
BrandonScheitlin <Brandon_scheitlin@baylor.edu>
2014-11-22 14:25:27 -0600

    Update brick.h
    BRANDON'S BALL.H
    11/22/2014

--- brick.h
+++ brick.h
@@ -1,21 +1,27 @@
 #ifndef BRICK_H_INCLUDED
 #define BRICK_H_INCLUDED
 
+#include "gLibrary.h"
+
 class brick
 {
     public:
-    brick(int w = 20, ink bc = darkgreen);
-    void setX(double);
-    void setY(double);
-    double getX();
-    double getY();
-    void eraseBr(Plotter&);
+    brick(int w = 20, int r = 3, ink = white, Point = Point(), char = SQUARE);
+    Point getLoc ();
+    void draw(Plotter&);
+    void setLoc(Point);
+    bool Active(Plotter&);
+    ink getColor();
+    void setColor(ink a);
+    char getShape();
 
     private:
-    int width;
+    int width, rows;
     Point loc;
     ink color;
-    int cols, rows;
+    bool active;
+    int hitCount;
+    char shape;
 };
 
 #endif // BRICK_H_INCLUDED
